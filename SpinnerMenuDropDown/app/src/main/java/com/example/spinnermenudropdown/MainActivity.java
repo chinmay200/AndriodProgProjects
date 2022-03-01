45dda032b862a6857ff40ad16a78b978b0fd8d14
Chinmay Borah <chinmay205765@keshav.du.ac.in>
2022-03-02 01:31:24 +0530

    Andriod Spinner

--- SpinnerMenuDropDown/app/src/main/java/com/example/spinnermenudropdown/MainActivity.java
+++ SpinnerMenuDropDown/app/src/main/java/com/example/spinnermenudropdown/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.spinnermenudropdown;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -17,6 +18,7 @@ import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     TextView greet;
+    int i = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -31,16 +33,33 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         greet = findViewById(R.id.greet);
 
         ArrayAdapter <CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.items, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
     }
 
+    @SuppressLint("SetTextI18n")
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
 //        Toast.makeText(this, i , Toast.LENGTH_SHORT).show();
-        
+        switch (i){
+
+                case 0:
+                greet.setText("Apple");
+                break;
+                case 1:
+                greet.setText("Orange");
+                break;
+                case 2:
+                greet.setText("Grape");
+                break;
+                case 3:
+                greet.setText("Banana");
+                break;
+            default:
+                greet.setText("Yolo");
+        }
     }
 
     @Override
