f837c13c54e3fbbf196914a9b10396df6c0c522f
Chinmay Borah <chinmay205765@keshav.du.ac.in>
2022-03-06 09:29:20 +0530

    Optimised popUp menu image view

--- PopUpMenu/app/src/main/java/com/example/popupmenu/MainActivity.java
+++ PopUpMenu/app/src/main/java/com/example/popupmenu/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -14,11 +15,13 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
 
     TextView fruit;
+    ImageView img;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         fruit = findViewById(R.id.fruit);
+        img = findViewById(R.id.image);
     }
     
     public void popupmenu(View v){
@@ -38,10 +41,7 @@ public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuI
                 Toast.makeText(this, "Apple", Toast.LENGTH_SHORT).show();
 
                 findViewById(R.id.dis).setBackgroundColor(getResources().getColor(R.color.red));
-                findViewById(R.id.bananaimg).setVisibility(View.INVISIBLE);
-                findViewById(R.id.grapeimg).setVisibility(View.INVISIBLE);
-                findViewById(R.id.orangeimg).setVisibility(View.INVISIBLE);
-                findViewById(R.id.appleimg).setVisibility(View.VISIBLE);
+                img.setImageResource(R.drawable.apple);
 
                 fruit.setText("Apple");
                 break;
@@ -49,30 +49,21 @@ public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuI
                 Toast.makeText(this, "Orange", Toast.LENGTH_SHORT).show();
 
                 findViewById(R.id.dis).setBackgroundColor(getResources().getColor(R.color.orange));
-                findViewById(R.id.bananaimg).setVisibility(View.INVISIBLE);
-                findViewById(R.id.grapeimg).setVisibility(View.INVISIBLE);
-                findViewById(R.id.appleimg).setVisibility(View.INVISIBLE);
-                findViewById(R.id.orangeimg).setVisibility(View.VISIBLE);
+                img.setImageResource(R.drawable.orange);
                 fruit.setText("Orange");
                 break;
             case R.id.grape:
                 Toast.makeText(this, "Grape", Toast.LENGTH_SHORT).show();
 
                 findViewById(R.id.dis).setBackgroundColor(getResources().getColor(R.color.green));
-                findViewById(R.id.bananaimg).setVisibility(View.INVISIBLE);
-                findViewById(R.id.appleimg).setVisibility(View.INVISIBLE);
-                findViewById(R.id.orangeimg).setVisibility(View.INVISIBLE);
-                findViewById(R.id.grapeimg).setVisibility(View.VISIBLE);
+                img.setImageResource(R.drawable.grape);
                 fruit.setText("Grape");
                 break;
             case R.id.banana:
                 Toast.makeText(this, "Banana", Toast.LENGTH_SHORT).show();
 
                 findViewById(R.id.dis).setBackgroundColor(getResources().getColor(R.color.yellow));
-                findViewById(R.id.appleimg).setVisibility(View.INVISIBLE);
-                findViewById(R.id.orangeimg).setVisibility(View.INVISIBLE);
-                findViewById(R.id.grapeimg).setVisibility(View.INVISIBLE);
-                findViewById(R.id.bananaimg).setVisibility(View.VISIBLE);
+                img.setImageResource(R.drawable.banana);
                 fruit.setText("Banana");
                 break;
         }
