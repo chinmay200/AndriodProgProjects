46f4260c190ef4f5453e3f258e5d28859a398b9e
Chinmay Borah <chinmay205765@keshav.du.ac.in>
2022-03-06 11:10:37 +0530

    Used shared preference to set theme

--- PopUpMenu/app/src/main/java/com/example/popupmenu/MainActivity.java
+++ PopUpMenu/app/src/main/java/com/example/popupmenu/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.popupmenu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
@@ -22,6 +24,28 @@ public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuI
         setContentView(R.layout.activity_main);
         fruit = findViewById(R.id.fruit);
         img = findViewById(R.id.image);
+
+        SharedPreferences shrd = getSharedPreferences("fruit" , MODE_PRIVATE);
+        String val = shrd.getString("fruit" , " ");
+        fruit.setText(val);
+        SharedPreferences bg = getSharedPreferences("bg" , MODE_PRIVATE);
+        String col = bg.getString("bgcolor" , "white");
+
+        if(col.equals("red")){
+            findViewById(R.id.dis).setBackgroundColor(getResources().getColor(R.color.red));
+        }
+        else if(col.equals("orange")){
+            findViewById(R.id.dis).setBackgroundColor(getResources().getColor(R.color.orange));
+        }
+        else if(col.equals("yellow")){
+            findViewById(R.id.dis).setBackgroundColor(getResources().getColor(R.color.yellow));
+        }
+        else if(col.equals("green")){
+            findViewById(R.id.dis).setBackgroundColor(getResources().getColor(R.color.green));
+        }
+        else {
+            findViewById(R.id.dis).setBackgroundColor(getResources().getColor(R.color.black));
+        }
     }
     
     public void popupmenu(View v){
@@ -42,7 +66,10 @@ public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuI
 
                 findViewById(R.id.dis).setBackgroundColor(getResources().getColor(R.color.red));
                 img.setImageResource(R.drawable.apple);
-
+                SharedPreferences shrd = getSharedPreferences("bg" , MODE_PRIVATE);
+                SharedPreferences.Editor editor= shrd.edit();
+                editor.putString("bgcolor" , "red");
+                editor.apply();
                 fruit.setText("Apple");
                 break;
             case R.id.orange:
@@ -50,6 +77,10 @@ public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuI
 
                 findViewById(R.id.dis).setBackgroundColor(getResources().getColor(R.color.orange));
                 img.setImageResource(R.drawable.orange);
+                shrd = getSharedPreferences("bg" , MODE_PRIVATE);
+                editor= shrd.edit();
+                editor.putString("bgcolor" , "orange");
+                editor.apply();
                 fruit.setText("Orange");
                 break;
             case R.id.grape:
@@ -57,6 +88,10 @@ public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuI
 
                 findViewById(R.id.dis).setBackgroundColor(getResources().getColor(R.color.green));
                 img.setImageResource(R.drawable.grape);
+                shrd = getSharedPreferences("bg" , MODE_PRIVATE);
+                editor= shrd.edit();
+                editor.putString("bgcolor" , "green");
+                editor.apply();
                 fruit.setText("Grape");
                 break;
             case R.id.banana:
@@ -64,9 +99,17 @@ public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuI
 
                 findViewById(R.id.dis).setBackgroundColor(getResources().getColor(R.color.yellow));
                 img.setImageResource(R.drawable.banana);
+                shrd = getSharedPreferences("bg" , MODE_PRIVATE);
+                editor= shrd.edit();
+                editor.putString("bgcolor" , "yellow");
+                editor.apply();
                 fruit.setText("Banana");
                 break;
         }
+        SharedPreferences shrd = getSharedPreferences("fruit" , MODE_PRIVATE);
+        SharedPreferences.Editor editor= shrd.edit();
+        editor.putString("fruit" , fruit.getText().toString());
+        editor.apply();
         return false;
     }
 }
