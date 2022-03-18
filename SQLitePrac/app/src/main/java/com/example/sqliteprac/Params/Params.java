c6c4c1143c13c7602c6e104ec3bbcfea1de88440
Chinmay Borah <chinmay205765@keshav.du.ac.in>
2022-03-18 16:37:43 +0530

    SQLite prac

---
+++ SQLitePrac/app/src/main/java/com/example/sqliteprac/Params/Params.java
@@ -0,0 +1,11 @@
+package com.example.sqliteprac.Params;
+
+public class Params {
+    public static final int DB_VERSION = 1;
+    public static final String DB_NAME = "toys";
+
+    public static final String KEY_ID = "id";
+    public static final String KEY_NAME = "toy";
+    public static final String KEY_TYPE = "type";
+
+}
---
+++ SQLitePrac/app/src/main/java/com/example/sqliteprac/data/MyDbHandler.java
@@ -0,0 +1,25 @@
+package com.example.sqliteprac.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import com.example.sqliteprac.Params.Params;
+
+public class MyDbHandler extends SQLiteOpenHelper {
+    public MyDbHandler(@Nullable Context context) {
+        super(context, Params.DB_NAME, null, Params.DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
