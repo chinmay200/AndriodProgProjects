72fb8078a38596df4c42d50022a400fe140668e2
Chinmay Borah <chinmay205765@keshav.du.ac.in>
2022-04-16 15:02:07 +0530

    Broadcast services

---
+++ BroadcasReciever/app/src/main/java/com/example/broadcasreciever/Reciver.java
@@ -0,0 +1,17 @@
+package com.example.broadcasreciever;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class Reciver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        Toast.makeText(context, "This is broadcast reciever", Toast.LENGTH_SHORT).show();
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
---
+++ BroadcastSender/app/src/main/java/com/example/broadcastsender/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.broadcastsender;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button b;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b = findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction("com.example.broadcastsender");
+                intent.setFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
+                sendBroadcast(intent); 
+            }
+        });
+    }
+}
